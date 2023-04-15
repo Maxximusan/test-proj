2381a0383e0b62be1fdcd1bccb2a37e7bea2e1e5
Maxximusan <dragonstonecave@gmail.com>
2023-04-15 19:23:35 +0300

    3й - пробная пагинация

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import { CardItem } from "../CardItem/CardItem";
 import users from "../../users.json";
 import { List } from "./cardList.styled";
+import { LoadMoreBtn } from "../LoadMoreBtn/LoadMoreBtn";
 
 export const CardList = () => {
   console.log(users.length);
+  const [needQuantityForPage, setNeedQuantityForPage] = useState(4);
+
+  const sliceUsers = users.slice(0, needQuantityForPage);
+
+  const loadMore = () => {
+    setNeedQuantityForPage((prevState) => prevState + needQuantityForPage);
+  };
   return (
-    <List>
-      {users.map((user) => (
-        <CardItem key={user.id} card={user} />
-      ))}
-    </List>
+    <section>
+      <List>
+        {sliceUsers.map((user) => (
+          <CardItem key={user.id} card={user} />
+        ))}
+      </List>
+
+      <LoadMoreBtn loadMore={loadMore} />
+    </section>
   );
 };
