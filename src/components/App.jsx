a94996dcc76b810ea2a7bb6745c4ab62c6176837
Maxximusan <dragonstonecave@gmail.com>
2023-04-17 12:55:40 +0300

    4.1 - mockApi вместо users.json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,44 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import * as SC from "./App.styled";
 import { CardList } from "./CardList/CardList";
-import users from "../users.json";
+// import users from "../users.json";
 import { LoadMoreBtn } from "./LoadMoreBtn/LoadMoreBtn";
+import * as MockApi from "../Api/mockapiBackend";
 
 export const App = () => {
-  console.log(users.length);
+  // console.log(users.length);
+
+  const [ussers, setUssers] = useState([]);
+
   const [needQuantityForPage, setNeedQuantityForPage] = useState(8);
 
-  const sliceUsers = users.slice(0, needQuantityForPage);
+  // const sliceUsers = users.slice(0, needQuantityForPage);
+
+  // const loadMore = () => {
+  //   setNeedQuantityForPage((prevState) => prevState + needQuantityForPage);
+  // };
+
+  useEffect(() => {
+    async function usersFetch() {
+      try {
+        const userss = await MockApi.fetchUsers();
+        console.log(userss);
+        setUssers(userss);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    usersFetch();
+  }, []);
+
+  const sliceUsers = ussers.slice(0, needQuantityForPage);
+  console.log(sliceUsers);
+  console.log(ussers);
 
   const loadMore = () => {
     setNeedQuantityForPage((prevState) => prevState + needQuantityForPage);
   };
+
   return (
     <SC.MainContainer>
       <CardList users={sliceUsers} />
